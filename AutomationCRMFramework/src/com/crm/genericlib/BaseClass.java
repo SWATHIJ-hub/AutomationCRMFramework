a3e7ec799eb9b83a2d14b747970f48b6cfae5ba9
SWATHIJ-hub <60400141+SWATHIJ-hub@users.noreply.github.com>
2020-01-30 10:14:10 +0530

    changed

--- AutomationCRMFramework/src/com/crm/genericlib/BaseClass.java
+++ AutomationCRMFramework/src/com/crm/genericlib/BaseClass.java
@@ -11,6 +11,6 @@ public class BaseClass {
 		System.out.println("from BSs");
 		System.out.println("from CSS");
                  System.out.print("Form gss");
-		System.out.print("Form gss");
+		System.out.print("Form g");
 	}
 }
